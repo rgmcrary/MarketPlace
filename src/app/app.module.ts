38d12545043b2b15244af4e5a5b1776741d8939e
Rich McRary <rgmcrary@gmail.com>
2018-05-19 11:52:39 -0400

    Created Page Compnonents and updated Mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { ManagerComponent } from './manager/manager.component';
+import { SupervisorComponent } from './supervisor/supervisor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    ManagerComponent,
+    SupervisorComponent
   ],
   imports: [
     BrowserModule
